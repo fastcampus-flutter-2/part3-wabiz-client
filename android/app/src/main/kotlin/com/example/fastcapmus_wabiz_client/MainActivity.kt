600c8024386eee6e36bc0b1cf2fbbe0df1941faf
Dreamwalker <aristojeff@gmail.com>
2025-06-21 19:59:58 +0900

    update platform codes

---
+++ android/app/src/main/kotlin/com/example/fastcapmus_wabiz_client/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.fastcapmus_wabiz_client
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
--- android/app/src/main/kotlin/com/fastcamups/fastcapmus_wabiz_client/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.fastcamups.fastcapmus_wabiz_client
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
